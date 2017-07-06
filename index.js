22e772877ec16cfde93601fde55d6f82653fb9a1
N0taN3rd <n0tan3rd@gmail.com>
2017-07-06 00:41:15 -0400

    bump version and updated to crawl same-domain and all links updated writers project is now MIT licenced

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 module.exports = require('./lib/parsers')
+module.exports.RemoteChromeWARCWriter = require('./lib/writers/remoteChrome')
+module.exports.ElectronWARCWriter = require('./lib/writers/electron')
