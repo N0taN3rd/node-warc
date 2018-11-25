5ae981224209906e95810bd2e54520c63e032957
John Berlin <n0tan3rd@gmail.com>
2018-11-25 01:16:58 -0500

    Updates

--- script/inherited-summary.js
+++ script/inherited-summary.js
@@ -1,28 +1,28 @@
-(function () {
-  function toggle (ev) {
-    var button = ev.target
-    var parent = ev.target.parentElement
-    while (parent) {
-      if (parent.tagName === 'TABLE' && parent.classList.contains('summary')) break
-      parent = parent.parentElement
+(function(){
+  function toggle(ev) {
+    var button = ev.target;
+    var parent = ev.target.parentElement;
+    while(parent) {
+      if (parent.tagName === 'TABLE' && parent.classList.contains('summary')) break;
+      parent = parent.parentElement;
     }
 
-    if (!parent) return
+    if (!parent) return;
 
-    var tbody = parent.querySelector('tbody')
+    var tbody = parent.querySelector('tbody');
     if (button.classList.contains('opened')) {
-      button.classList.remove('opened')
-      button.classList.add('closed')
-      tbody.style.display = 'none'
+      button.classList.remove('opened');
+      button.classList.add('closed');
+      tbody.style.display = 'none';
     } else {
-      button.classList.remove('closed')
-      button.classList.add('opened')
-      tbody.style.display = 'block'
+      button.classList.remove('closed');
+      button.classList.add('opened');
+      tbody.style.display = 'block';
     }
   }
 
-  var buttons = document.querySelectorAll('.inherited-summary thead .toggle')
+  var buttons = document.querySelectorAll('.inherited-summary thead .toggle');
   for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', toggle)
+    buttons[i].addEventListener('click', toggle);
   }
-})()
+})();
--- script/test-summary.js
+++ script/test-summary.js
@@ -1,54 +1,54 @@
-(function () {
-  function toggle (ev) {
-    var button = ev.target
-    var parent = ev.target.parentElement
-    while (parent) {
-      if (parent.tagName === 'TR' && parent.classList.contains('test-interface')) break
-      parent = parent.parentElement
+(function(){
+  function toggle(ev) {
+    var button = ev.target;
+    var parent = ev.target.parentElement;
+    while(parent) {
+      if (parent.tagName === 'TR' && parent.classList.contains('test-interface')) break;
+      parent = parent.parentElement;
     }
 
-    if (!parent) return
+    if (!parent) return;
 
-    var direction
+    var direction;
     if (button.classList.contains('opened')) {
-      button.classList.remove('opened')
-      button.classList.add('closed')
-      direction = 'closed'
+      button.classList.remove('opened');
+      button.classList.add('closed');
+      direction = 'closed';
     } else {
-      button.classList.remove('closed')
-      button.classList.add('opened')
-      direction = 'opened'
+      button.classList.remove('closed');
+      button.classList.add('opened');
+      direction = 'opened';
     }
 
-    var targetDepth = parseInt(parent.dataset.testDepth, 10) + 1
-    var nextElement = parent.nextElementSibling
+    var targetDepth = parseInt(parent.dataset.testDepth, 10) + 1;
+    var nextElement = parent.nextElementSibling;
     while (nextElement) {
-      var depth = parseInt(nextElement.dataset.testDepth, 10)
+      var depth = parseInt(nextElement.dataset.testDepth, 10);
       if (depth >= targetDepth) {
         if (direction === 'opened') {
-          if (depth === targetDepth) nextElement.style.display = ''
+          if (depth === targetDepth)  nextElement.style.display = '';
         } else if (direction === 'closed') {
-          nextElement.style.display = 'none'
-          var innerButton = nextElement.querySelector('.toggle')
+          nextElement.style.display = 'none';
+          var innerButton = nextElement.querySelector('.toggle');
           if (innerButton && innerButton.classList.contains('opened')) {
-            innerButton.classList.remove('opened')
-            innerButton.classList.add('closed')
+            innerButton.classList.remove('opened');
+            innerButton.classList.add('closed');
           }
         }
       } else {
-        break
+        break;
       }
-      nextElement = nextElement.nextElementSibling
+      nextElement = nextElement.nextElementSibling;
     }
   }
 
-  var buttons = document.querySelectorAll('.test-summary tr.test-interface .toggle')
+  var buttons = document.querySelectorAll('.test-summary tr.test-interface .toggle');
   for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', toggle)
+    buttons[i].addEventListener('click', toggle);
   }
 
-  var topDescribes = document.querySelectorAll('.test-summary tr[data-test-depth="0"]')
+  var topDescribes = document.querySelectorAll('.test-summary tr[data-test-depth="0"]');
   for (var i = 0; i < topDescribes.length; i++) {
-    topDescribes[i].style.display = ''
+    topDescribes[i].style.display = '';
   }
-})()
+})();
